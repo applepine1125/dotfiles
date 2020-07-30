42a82c0859fe912c80be1aa97471bf6b71190cf3
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2020-07-30 11:01:50 +0900

    fix tmux powerline settings

--- tmux/.tmux/mytheme.sh
+++ tmux/.tmux/mytheme.sh
@@ -22,12 +22,10 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
   TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
   "tmux_session_info 148 234" \
-    "hostname 33 0" \
-    "ifstat 30 255" \
-    "ifstat_sys 30 255" \
+    # "ifstat_sys 30 255" \
     "lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
     "wan_ip 24 255" \
-    "vcs_branch 29 88" \
+    "vcs_branch 29 255" \
     "vcs_compare 60 255" \
     "vcs_staged 64 255" \
     "vcs_modified 9 255" \
@@ -37,10 +35,7 @@ fi
 
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
   TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
-    "pwd 100 240" \
-    "cpu 240 136" \
-    "tmux_mem_cpu_load 235 136" \
-    "battery 137 127" \
+    # "hostname 33 0" \
     "date_day 235 136" \
     "date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
     "time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
